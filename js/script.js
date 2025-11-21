2b21827fa1b91f7886509d7904fc12881690371d
Ashley <126aperezmorales@frhsd.com>
2025-11-21 09:43:55 -0500

    2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let msg = document.getElementById("secretText991");
 msg.style.visibility = "visible";
 msg.textContent = "You found me using the DOM!";
@@ -12,3 +11,44 @@ btn.onclick = function() {
   patch.style.backgroundColor = "pink";
   btn.textContent = count;
 };
+
+let box = document.getElementById("box7");
+let big = false;
+
+box.onmouseenter = function() {
+  box.style.borderRadius = "50%";
+};
+
+box.onmouseleave = function() {
+  box.style.borderRadius = "0";
+};
+
+box.ondblclick = function() {
+  if (big) {
+    box.style.width = "100px";
+    box.style.height = "100px";
+  } else {
+    box.style.width = "200px";
+    box.style.height = "200px";
+  }
+  big = !big;
+};
+
+let input = document.getElementById("decoderInput");
+let output = document.getElementById("decodedOutput");
+
+input.oninput = function() {
+  if (input.value.includes("x")) {
+    output.textContent = "Forbidden letter detected.";
+  } else {
+    output.textContent = input.value;
+  }
+};
+
+let title = document.getElementById("glitchTitle");
+
+setTimeout(function() {
+  title.textContent = "DOM Restored!";
+  title.style.color = "red";
+  title.style.fontSize = "40px";
+}, 3000);
